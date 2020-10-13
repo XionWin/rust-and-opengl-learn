6a47ca3071914baea37bcb92b00c5b265a5d9e7c
Win <xionwin@hotmail.com>
2020-10-13 11:43:58 -0400

    Update

---
+++ learn-03-shader/src/main.rs
@@ -0,0 +1,62 @@
+extern crate sdl2;
+extern crate gl;
+
+pub mod shader;
+
+fn main() {
+    
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+
+
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 1);
+
+    let window = video_subsystem
+        .window("Game window 2", 900, 700)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
+
+
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+
+    use std::ffi::CString;
+    let vert_shader =
+        shader::Shader::from_vert_source(&CString::new(include_str!("triangle.vert")).unwrap())
+            .unwrap();
+
+    let frag_shader =
+    shader::Shader::from_frag_source(&CString::new(include_str!("triangle.frag")).unwrap())
+            .unwrap();
+
+    let shader_program = shader::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
+
+    shader_program.set_used();
+
+    unsafe {
+        gl::Viewport(0, 0, 900, 700);
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'main,
+                _ => {}
+            }
+        }
+
+        // render window contents here
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
+    }
+}
---
+++ learn-04-triangle/src/shader/mod.rs
@@ -0,0 +1,7 @@
+mod shader;
+mod program;
+mod utility;
+
+pub use shader::*;
+pub use program::*;
+pub use utility::*;
