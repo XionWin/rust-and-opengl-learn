72d4fdc42ae68b823f7e61fbbc964b8edbc63fb4
Xion <win@Xions-MacBook.local>
2020-10-18 21:55:34 +0800

    Update

--- learn-01-window/src/main.rs
+++ learn-01-window/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
     let _window = video_subsystem
-        .window("Game window1", 900, 700)
+        .window("Game", 900, 700)
         .resizable()
         .build()
         .unwrap();
